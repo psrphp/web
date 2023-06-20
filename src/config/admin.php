77d7347c96a3a00c4dae1fedb8d91e2ae28721b6
John Doe <johndoe@example.com>
2023-06-20 11:22:56 +0800

    1

--- src/config/admin.php
+++ src/config/admin.php
@@ -1,29 +1,14 @@
 <?php
 
-use App\Psrphp\Admin\Model\Account;
 use App\Psrphp\Web\Http\Config\Index;
 use App\Psrphp\Web\Http\Page\Index as PageIndex;
-use PsrPHP\Router\Router;
-use PsrPHP\Framework\Framework;
 
 return [
-    'menus' => Framework::execute(function (
-        Account $account,
-        Router $router
-    ): array {
-        $menus = [];
-        if ($account->checkAuth(Index::class)) {
-            $menus[] = [
-                'title' => '网站设置',
-                'url' => $router->build('/psrphp/web/config/index'),
-            ];
-        }
-        if ($account->checkAuth(PageIndex::class)) {
-            $menus[] = [
-                'title' => '页面管理',
-                'url' => $router->build('/psrphp/web/page/index'),
-            ];
-        }
-        return $menus;
-    })
+    'menus' => [[
+        'title' => '网站设置',
+        'node' => Index::class
+    ], [
+        'title' => '页面管理',
+        'node' => PageIndex::class
+    ]]
 ];
